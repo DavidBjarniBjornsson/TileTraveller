58d38c3da4f117c3dc97004dbd6e145ca3322b61
David Bjornsson <David@Davids-MacBook-Pro-2.local>
2018-09-19 19:29:13 +0200

    Positions assigned for each tile

--- TileTraveller.py
+++ TileTraveller.py
@@ -4,3 +4,15 @@
 # Player's new tyle displayed
 # Every tyle is its own variable with its own possible inputs
 # When the player gets to the tile 3,1 the while loop stops and the player wins 
+
+tile_1 = 1.1
+tile_2 = 1.2
+tile_3 = 1.3
+tile_4 = 2.3
+tile_5 = 2.2
+tile_6 = 2.1
+tile_7 = 3.3
+tile_8 = 3.2
+tile_9 = 3.1
+position = tile1
+win = False
